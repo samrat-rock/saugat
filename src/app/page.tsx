1d58f826dafad4f873f319aea485a1ecd8237b84
samrat-rock <samratpandey715@gmail.com>
2025-10-17 10:15:52 +0545

    added second navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Electronic from '@/MainComponents/Electronics/Electronic'
 import Navbar from '@/MainComponents/Navbar/Navbar'
+import SecondNavBar from '@/MainComponents/SecondNavBar/SecondNavBar'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-
+     <SecondNavBar/>
     <Electronic/>
+    
     </>
   )
 }
